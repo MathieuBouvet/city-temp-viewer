babefa027eca3355cbe67abb3233e642f0cd1806
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-04 12:33:36 +0100

    Set up the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import CityInput from "./components/CityInput";
+import TemperatureViewer from "./components/TemperatureViewer";
+
 import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
   return (
     <div className="App">
       <header className="App-header">City Temp Viewer</header>
+      {!city ? (
+        <CityInput onSubmitCity={setCity} />
+      ) : (
+        <TemperatureViewer city={city} onReset={() => setCity("")} />
+      )}
     </div>
   );
 }
